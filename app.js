7158b4f1a636a6154ac6644d9964e84411deccc9
akseyh <akgulmuhammed@live.com>
2019-03-03 17:36:07 +0300

    dosyalar eklendi

---
+++ app.js
@@ -0,0 +1,66 @@
+let isIslemDefine = false,islemText,ilkSayi,ikinciSayi;
+function yazdir(sayi){
+  if(isIslemDefine)
+    document.getElementById("ekran").innerHTML = "";
+  
+  isIslemDefine = 0;
+  document.getElementById("ekran").innerHTML += sayi;
+}
+
+function temizle(){
+  document.getElementById("ekran").innerHTML = "";
+  ilkSayi = "";
+  ikinciSayi = "";
+  isIslemDefine = false;
+}
+
+function sil(){
+  let screenText = document.getElementById("ekran").innerHTML;
+  document.getElementById("ekran").innerHTML = screenText.substring(0,screenText.length-1);
+}
+
+function islem(islem){
+  
+  if(islem == "-" && document.getElementById("ekran").innerHTML.trim() == ""){
+    yazdir(islem);
+    return 0;
+  }
+  
+  if(islem == "." && document.getElementById("ekran").innerHTML.indexOf(".") == -1 && document.getElementById("ekran").innerHTML != "" ){
+    yazdir(islem);
+    return 0;
+  } else if(islem == ".") return 0;
+  
+  ilkSayi = Number(document.getElementById("ekran").innerHTML);
+  isIslemDefine = true;
+  islemText = islem;
+}  
+
+function esittir(){
+  ikinciSayi = Number(document.getElementById("ekran").innerHTML);
+  
+  switch(islemText){
+    case "+":
+      document.getElementById("ekran").innerHTML = ilkSayi+ikinciSayi;
+      isIslemDefine = false;
+      break;
+    case "-":
+      document.getElementById("ekran").innerHTML = ilkSayi-ikinciSayi;
+      isIslemDefine = false;
+      break;
+    case "x":
+      document.getElementById("ekran").innerHTML = ilkSayi*ikinciSayi;
+      isIslemDefine = false;
+      break;
+    case "/":
+      document.getElementById("ekran").innerHTML = ilkSayi / ikinciSayi;
+      isIslemDefine = false;
+      break;
+    case "%":
+      document.getElementById("ekran").innerHTML = ilkSayi * ikinciSayi / 100;
+      isIslemDefine = false;
+      break;
+  }
+  ilkSayi = "";
+  ikiniSayi = "";
+}
